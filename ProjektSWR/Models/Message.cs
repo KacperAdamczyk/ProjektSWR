f8ecc65f3d2a2108c12a726d210cbd80fc3f6ba2
KacperAdamczyk <kaacpers@gmail.com>
2017-04-09 20:23:52 +0200

    DB update + doc

---
+++ ProjektSWR/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ProjektSWR.Models
+{
+    public class Message
+    {
+        public int ID_wiadomosci { get; set; }
+        public int ID_nadawcy { get; set; }
+        public int ID_odbiorcy { get; set; }
+        public string Temat { get; set; }
+        public string Tresc { get; set; }
+        public DateTime Data_nadania { get; set; }
+        public DateTime Data_odbioru { get; set; }
+        public bool Archiwizacja { get; set; }
+        public int ID_odpowiedzi { get; set; }
+    }
+
+    public class MessageDBContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+    }
+}
