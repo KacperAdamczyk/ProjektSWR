c15c42cd86bf7fe1b79f1f6bb0578e1e1733d94f
misiek4895 <michaaal.b@gmail.com>
2017-05-07 00:20:18 +0200

    Implementacja bazy danych (niedokończona)

---
+++ ProjektSWR/Models/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+
+namespace ProjektSWR.Models
+{
+    public class GlobalEvent
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Coordinator { get; set; }
+
+    }
+
+    public class PrivateEvent
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+    }
+
+    public class Event
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required] public string Title { get; set; }
+        [Required] public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+
+    }
+}
---
+++ ProjektSWR/Models/Notification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektSWR.Models
+{
+    public class Notification
+    {
+    }
+}
