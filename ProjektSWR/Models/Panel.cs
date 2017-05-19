4fefec2257c7bee85e4a07728cfab26e3e827a01
guthx <shao34@gmail.com>
2017-05-19 16:40:38 +0200

    wstepna fukcjonalnosc panelu

---
+++ ProjektSWR/Models/Panel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjektSWR.Models
+{
+    public class ManageUsersModel
+    {
+        [EmailAddress]
+        [Display(Name = "e-mail: (@prz.edu.pl)")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Katedra")]
+        public string CathedralName { get; set; }
+
+        public string Id { get; set; }
+    }
+}
