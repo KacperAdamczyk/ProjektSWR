aa362e9e0e98ca6206e5e2b5c4cbf1870412ac1e
Paweł Balcerek <pbalcerek96@o2.pl>
2017-05-21 19:39:11 +0200

    Profil, panel

--- ProjektSWR/Controllers/ProfileController.cs
+++ ProjektSWR/Controllers/ProfileController.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ProjektSWR.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
 
 namespace ProjektSWR.Controllers
 {
     public class ProfileController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Profile
         public ActionResult Index()
         {
-            return View();
+            var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+            ViewBag.Message = "Panel użytkownika";
+
+            var userId = User.Identity.GetUserId();
+            var m = new ProfileModel
+            {
+                FirstName = db.Users.Find(userId).FirstName,
+                LastName = db.Users.Find(userId).LastName,
+                AcademicDegree = db.Users.Find(userId).AcademicDegree,
+                Photo = db.Users.Find(userId).Photo,
+                //DateOfBirth = db.Users.Find(userId).DateOfBirth,
+                Description = db.Users.Find(userId).Description,
+                Email = db.Users.Find(userId).Email,
+                PhoneNumber = db.Users.Find(userId).PhoneNumber,
+                CathedralName = db.Cathedrals.Find(1).Department,
+            };
+
+            return View(m);
         }
+
+
     }
 }
---
+++ ProjektSWR/Models/ProfileModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+namespace ProjektSWR.Models
+{
+    public class ProfileModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string AcademicDegree { get; set; }
+
+        public string Photo { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Katedra: ")]
+        public string CathedralName { get; set; }
+    }
+}
