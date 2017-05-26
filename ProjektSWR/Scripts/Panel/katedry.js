25c97dbea55b609fa0683350e02b7b389fd550ac
guthx <shao34@gmail.com>
2017-05-26 13:03:45 +0200

    Aktualizacja panelu admina

---
+++ ProjektSWR/Scripts/Panel/katedry.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $.getJSON("/Panel/Katedry", parseJSON3);
+
+
+});
+
+function parseJSON3(data3) {
+    //data2 = JSON.parse(data2);
+    var i;
+    for (i = 0; i < data3.length; i++) {
+        var nr = i + 1
+        var line = "<option>" + data3[i].Department + "</option>";
+        //var line = "<option value=" + "\"" + nr + "\">" +  " "  + "</option>";
+        $("#cathedrals_select").append(line);
+    }
+}
